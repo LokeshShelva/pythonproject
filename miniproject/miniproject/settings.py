9b07395c9c1e2ef7c8ffbd83079e661e9bc16331
Lokesh <5shelva19@gmail.com>
2021-05-14 11:17:27 +0530

    added main app and routes to home page

--- miniproject/miniproject/settings.py
+++ miniproject/miniproject/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.MainConfig',
     'user.apps.UserConfig',
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
