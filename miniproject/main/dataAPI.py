0a69bd233d73cbf6fdff250f870f78a146c07ade
Lokesh <5shelva19@gmail.com>
2021-05-16 20:18:12 +0530

    added CRUD functionality to database

--- miniproject/main/dataAPI.py
+++ miniproject/main/dataAPI.py
@@ -1,18 +1,41 @@
 
-def data_API(data):
-    cleaned = {
-        "name": data['graphTitle'],
-        "description": data['graphDescription'],
-        "xAxis": {
-            "name": data["xAxisName"],
-            "minRange": data["xAxisMinRange"],
-            "maxRange": data["xAxisMaxRange"]
-        },
-        "yAxis": {
-            "name": data["yAxisName"],
-            "minRange": data["yAxisMinRange"],
-            "maxRange": data["yAxisMaxRange"]
-        },
-    }
+def data_API(data, user):
+    if user:
+        cleaned = {
+            "name": data['graphTitle'],
+            "user": user,
+            "description": data['graphDescription'],
+            "xAxis": {
+                "name": data["xAxisName"],
+                "unit": data["xAxisUnit"],
+                "minRange": data["xAxisMinRange"],
+                "maxRange": data["xAxisMaxRange"]
+            },
+            "yAxis": {
+                "name": data["yAxisName"],
+                "unit": data["xAxisUnit"],
+                "minRange": data["yAxisMinRange"],
+                "maxRange": data["yAxisMaxRange"]
+            },
+            "data": []
+        }
+    else:
+        cleaned = {
+            "name": data['graphTitle'],
+            "description": data['graphDescription'],
+            "xAxis": {
+                "name": data["xAxisName"],
+                "unit": data["xAxisUnit"],
+                "minRange": data["xAxisMinRange"],
+                "maxRange": data["xAxisMaxRange"]
+            },
+            "yAxis": {
+                "name": data["yAxisName"],
+                "unit": data["xAxisUnit"],
+                "minRange": data["yAxisMinRange"],
+                "maxRange": data["yAxisMaxRange"]
+            },
+            "data": []
+        }
 
     return cleaned
--- miniproject/main/urls.py
+++ miniproject/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('create/', views.create, name='create'),
-    path('plot/<str:plotId>', views.plot, name='plot'),
+    path('delete/', views.delete, name='delete'),
+    path('add/', views.add, name='add')
 ]
